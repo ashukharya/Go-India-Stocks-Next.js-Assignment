ebb4725d7bad298b2b75e180a5b26aa197117b01
ashukharya <ashukharya139@gmail.com>
2024-03-22 17:09:47 +0530

    Second Commit

--- src/app/components/MarketStories/MarketStoriesCard.tsx
+++ src/app/components/MarketStories/MarketStoriesCard.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 import React from 'react'
 
 type MarketStoriesCardProps = {
-    data:MarketStoriesData;
+  data: MarketStoriesData;
 }
 
-function MarketStoriesCard({data}:MarketStoriesCardProps) {
+function MarketStoriesCard({ data }: MarketStoriesCardProps) {
   return (
     <div className='border-x border-t'>
       <div className='p-1'>
-        <Image src={'/images/placeholder-img.jpg'} width={400} height={300} alt='' className='w-full max-h-60 h-auto object-cover' loading='lazy'/>
+        <Image src={'/images/mountain.png'} width={400} height={300} alt='' className='w-full max-h-60 h-auto object-cover' loading='lazy' />
       </div>
       <div className='p-5 flex flex-col gap-3'>
         <h3 className='text-2xl font-semibold text-gray-800'>{data.title}</h3>
